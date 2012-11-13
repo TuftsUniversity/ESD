607cf2b54ecd606077b899bed5f69e4ba5d34c39
Deborah Kaplan <deborah.kaplan@tufts.edu>
2012-11-13 13:13:34 -0500

    adding the CORRECT license information

--- lib/ESD/Controller/DepositSubmission.pm
+++ lib/ESD/Controller/DepositSubmission.pm
@@ -831,16 +831,16 @@ Deborah Kaplan
 
 Copyright 2012 Tufts University
 
-CIDER is free software: you can redistribute it and/or modify
+ESD is free software: you can redistribute it and/or modify
 it under the terms of the GNU Affero General Public License as
 published by the Free Software Foundation, either version 3 of
 the License, or (at your option) any later version.
 
-CIDER is distributed in the hope that it will be useful, but
+ESD is distributed in the hope that it will be useful, but
 WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
 Affero General Public License for more details.
 
 You should have received a copy of the GNU Affero General Public
-License along with CIDER.  If not, see
+License along with ESD.  If not, see
 <http://www.gnu.org/licenses/>.
