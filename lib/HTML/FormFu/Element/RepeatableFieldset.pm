39b20f2b466f38d23f1d1e7c9115f4d929628d41
Deborah Kaplan <deborah.kaplan@tufts.edu>
2012-11-13 13:07:45 -0500

    EWS-29 adding Affero GNU public license to all the public code

--- lib/HTML/FormFu/Element/RepeatableFieldset.pm
+++ lib/HTML/FormFu/Element/RepeatableFieldset.pm
@@ -144,7 +144,21 @@ Doug Orleans, Appleseed Software Consulting <dougo@appleseed-sc.com>
 
 =head1 COPYRIGHT
 
-Copyright (c) 2009 by Tufts University.
+Copyright 2012 Tufts University
+
+CIDER is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as
+published by the Free Software Foundation, either version 3 of
+the License, or (at your option) any later version.
+
+CIDER is distributed in the hope that it will be useful, but
+WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public
+License along with CIDER.  If not, see
+<http://www.gnu.org/licenses/>.
 
 =cut
 
